cc865d9d12e4020af4c1698e6b8e32bf704f362e
jtu0528 <jtu6099@gmail.com>
2026-04-15 15:40:58 +0800

    PlayerDTO.java

--- src/main/java/com/tku/nba/PlayerDTO.java
+++ src/main/java/com/tku/nba/PlayerDTO.java
@@ -1,19 +1,24 @@
 package com.tku.nba;
-
 import java.util.List;
 
 public class PlayerDTO {
     private String fullName;
+    private String team;
+    private String season;
     private double pts;
     private double reb;
     private double ast;
     private String coreStyle;
-    private List<Double> ptsHistory; // 存放趨勢圖數據
+    private List<Double> ptsHistory;
 
     public PlayerDTO() {}
-
+    // Getter & Setter 
     public String getFullName() { return fullName; }
     public void setFullName(String fullName) { this.fullName = fullName; }
+    public String getTeam() { return team; }
+    public void setTeam(String team) { this.team = team; }
+    public String getSeason() { return season; }
+    public void setSeason(String season) { this.season = season; }
     public double getPts() { return pts; }
     public void setPts(double pts) { this.pts = pts; }
     public double getReb() { return reb; }
