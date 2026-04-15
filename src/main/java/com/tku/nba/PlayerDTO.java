a4bbbb3d28ab5f17be278daeeca33d08d96e35eb
jtu0528 <jtu6099@gmail.com>
2026-04-15 14:12:08 +0800

    PlayerDTO.java

--- src/main/java/com/tku/nba/PlayerDTO.java
+++ src/main/java/com/tku/nba/PlayerDTO.java
@@ -1,14 +1,21 @@
-package com.tku.nba.model;
+package com.tku.nba;
 
-import lombok.Data;
-
-@Data
 public class PlayerDTO {
     private String fullName;
-    private String team;
-    private double pts; // 場均得分
-    private double reb; // 場均籃板
-    private double ast; // 場均助攻
-    private String style; // 你的分析邏輯：得分機器/全能巨星
-    private String rating; // 評價
+    private double pts;
+    private double reb;
+    private double ast;
+    private String coreStyle;
+
+    // Getter & Setter (一定要有，Spring 才能讀取數據)
+    public String getFullName() { return fullName; }
+    public void setFullName(String fullName) { this.fullName = fullName; }
+    public double getPts() { return pts; }
+    public void setPts(double pts) { this.pts = pts; }
+    public double getReb() { return reb; }
+    public void setReb(double reb) { this.reb = reb; }
+    public double getAst() { return ast; }
+    public void setAst(double ast) { this.ast = ast; }
+    public String getCoreStyle() { return coreStyle; }
+    public void setCoreStyle(String coreStyle) { this.coreStyle = coreStyle; }
 }
